462b15938a61971cdf58769027ea37227a750560
Kygan <kygandao23@gmail.com>
2022-03-04 12:17:32 -0800

    initial commit

---
+++ src/test/java/com/company/RareBookStore/RareBookStoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.company.RareBookStore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RareBookStoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
