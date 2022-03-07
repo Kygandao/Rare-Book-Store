fc5c7f63f98753206b3538aace0bb33f0f757f4b
Maria Vallejo <maria.vallejo29@yahoo.com>
2022-03-07 13:20:27 -0600

    setup test and juinit 4 dependecy

---
+++ src/test/java/com/company/RareBookStore/repository/BookRepositoryTest.java
@@ -0,0 +1,12 @@
+package com.company.RareBookStore.repository;
+
+import org.junit.Before;
+
+import static org.junit.Assert.*;
+
+public class BookRepositoryTest {
+
+    @Before
+    public void setUp() throws Exception {
+    }
+}
