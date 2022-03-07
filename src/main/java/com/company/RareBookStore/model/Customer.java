a47f2186ea455c55c5fd7a3e483a995a78f56564
Maria Vallejo <maria.vallejo29@yahoo.com>
2022-03-07 10:24:55 -0600

    added customer model and repository

---
+++ src/main/java/com/company/RareBookStore/model/Customer.java
@@ -0,0 +1,132 @@
+package com.company.RareBookStore.model;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+import javax.persistence.*;
+import java.io.Serializable;
+import java.util.Objects;
+import java.util.Set;
+
+@Entity
+@JsonIgnoreProperties({ "hibernateLazyInitializer", "handler" })
+@Table(name = "customer")
+public class Customer  implements Serializable {
+
+    @Id
+    @Column(name = "customer_id")
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Integer id;
+
+    private String firstName;
+    private String lastName;
+    private String email;
+    private String phoneNumber;
+    private Address address;
+    private boolean vip;
+    private Set<Book> purchases;
+
+    public Customer() {
+    }
+
+    public Customer(Integer id, String firstName, String lastName, String email, String phoneNumber, Address address, boolean vip, Set<Book> purchases) {
+        this.id = id;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.email = email;
+        this.phoneNumber = phoneNumber;
+        this.address = address;
+        this.vip = vip;
+        this.purchases = purchases;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    public Address getAddress() {
+        return address;
+    }
+
+    public void setAddress(Address address) {
+        this.address = address;
+    }
+
+    public boolean isVip() {
+        return vip;
+    }
+
+    public void setVip(boolean vip) {
+        this.vip = vip;
+    }
+
+    public Set<Book> getPurchases() {
+        return purchases;
+    }
+
+    public void setPurchases(Set<Book> purchases) {
+        this.purchases = purchases;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Customer customer = (Customer) o;
+        return vip == customer.vip && Objects.equals(id, customer.id) && Objects.equals(firstName, customer.firstName) && Objects.equals(lastName, customer.lastName) && Objects.equals(email, customer.email) && Objects.equals(phoneNumber, customer.phoneNumber) && Objects.equals(address, customer.address) && Objects.equals(purchases, customer.purchases);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, firstName, lastName, email, phoneNumber, address, vip, purchases);
+    }
+
+    @Override
+    public String toString() {
+        return "Customer{" +
+                "id=" + id +
+                ", firstName='" + firstName + '\'' +
+                ", lastName='" + lastName + '\'' +
+                ", email='" + email + '\'' +
+                ", phoneNumber='" + phoneNumber + '\'' +
+                ", address=" + address +
+                ", vip=" + vip +
+                ", purchases=" + purchases +
+                '}';
+    }
+}
